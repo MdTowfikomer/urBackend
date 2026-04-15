4e6b3c7f6e905519d3913bf4a01c3cf9d659a229
yash-pouranik <yashpouranik124@gmail.com>
2026-04-15 20:57:47 +0530

    refactor(common): consolidate query building and cleanup QueryEngine

--- packages/common/src/utils/queryEngine.js
+++ packages/common/src/utils/queryEngine.js
@@ -4,10 +4,18 @@ class QueryEngine {
         this.queryString = queryString;
     }
 
-    filter() {
+    static EXCLUDED_FIELDS = ['page', 'sort', 'limit', 'fields', 'populate', 'expand', 'count'];
+
+    /**
+     * Builds a MongoDB query object from the query string parameters.
+     * Handles special suffixes like _gt, _gte, _lt, _lte.
+     * @param {boolean} excludeCount - Whether to explicitly exclude 'count' from the query (legacy parameter)
+     * @returns {Object} MongoDB query object
+     */
+    _buildMongoQuery(excludeCount = false) {
         const queryObj = { ...this.queryString };
-        const excludedFields = ['page', 'sort', 'limit', 'fields', 'populate', 'expand'];
-        excludedFields.forEach(el => delete queryObj[el]);
+        
+        QueryEngine.EXCLUDED_FIELDS.forEach(el => delete queryObj[el]);
 
         const mongoQuery = {};
         for (const key in queryObj) {
@@ -27,9 +35,11 @@ class QueryEngine {
                 mongoQuery[key] = queryObj[key];
             }
         }
-        
-        this.query = this.query.find(mongoQuery);
-        
+        return mongoQuery;
+    }
+
+    filter() {
+        this.query = this.query.find(this._buildMongoQuery(true));
         return this;
     }
 
@@ -48,6 +58,16 @@ class QueryEngine {
             this.query = this.query.sort('-createdAt');
         }
         
+        return this;
+    }
+
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select('-__v');
+        }
         return this;
     }
 
@@ -72,43 +92,10 @@ class QueryEngine {
         return this;
     }
 
-<<<<<<< HEAD
     async count() {
         // Clone the query to avoid affecting the original query's skip/limit
         return await this.query.model.countDocuments(this.query.getQuery());
     }
-=======
-   _buildMongoQuery(excludeCount = false) {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields', 'populate', 'expand'];
-    if (excludeCount) excludedFields.push('count');
-    excludedFields.forEach(el => delete queryObj[el]);
-    const mongoQuery = {};
-    for (const key in queryObj) {
-        if (key.endsWith('_gt')) {
-            const field = key.replace(/_gt$/, '');
-            mongoQuery[field] = { ...mongoQuery[field], $gt: queryObj[key] };
-        } else if (key.endsWith('_gte')) {
-            const field = key.replace(/_gte$/, '');
-            mongoQuery[field] = { ...mongoQuery[field], $gte: queryObj[key] };
-        } else if (key.endsWith('_lt')) {
-            const field = key.replace(/_lt$/, '');
-            mongoQuery[field] = { ...mongoQuery[field], $lt: queryObj[key] };
-        } else if (key.endsWith('_lte')) {
-            const field = key.replace(/_lte$/, '');
-            mongoQuery[field] = { ...mongoQuery[field], $lte: queryObj[key] };
-        } else {
-            mongoQuery[key] = queryObj[key];
-        }
-    }
-    return mongoQuery;
-}
-
-count() {
-    this.query = this.query.model.countDocuments(this._buildMongoQuery(true));
-    return this;
-}
->>>>>>> 3fb84f3e (feat: implement native client.db.count() support)
 }
 
 module.exports = QueryEngine;
